5cc0c95f82493264ae5bf83d40811826a99ae5e4
asahtik <asahtik@gmail.com>
2022-02-02 12:01:45 +0100

    Ok now build is fixed

--- src/ros_replacements/include/ros_replacements/status_output.h
+++ src/ros_replacements/include/ros_replacements/status_output.h
@@ -5,14 +5,10 @@
 #ifndef REPL_STATUS_H
 #define REPL_STATUS_H
 
-void format_string(const char*, ...);
-template<typename... Args>
-extern void OUTPUT_INFO(std::string, Args...);
+extern void OUTPUT_INFO(std::string, ...);
 
-template<typename... Args>
-extern void OUTPUT_WARNING(std::string, Args...);
+extern void OUTPUT_WARNING(std::string, ...);
 
-template<typename... Args>
-extern void OUTPUT_ERROR(std::string, Args...);
+extern void OUTPUT_ERROR(std::string, ...);
 
 #endif
