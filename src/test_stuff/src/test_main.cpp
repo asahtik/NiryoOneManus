5a7a728ae5df01cda6d1e0e31d2e015de8982418
asahtik <asahtik@gmail.com>
2022-02-03 10:19:59 +0100

    Robot calibrates, moves around and shuts down

--- src/test_stuff/src/test_main.cpp
+++ src/test_stuff/src/test_main.cpp
@@ -13,16 +13,17 @@ void controlLoop()
         // ros::Duration elapsed_time;
 
         // std::cout << mi->pos[0] << ", " << mi->pos[1] << ", " << mi->pos[2] << ", " << mi->pos[3] << ", " << mi->pos[4] << ", " << mi->pos[5] << ", " << mi->pos[6] << std::endl;
-
-        std::string err;
-        mi->comm->allowMotorsCalibrationToStart(1, err);
-
-        std::cout << err << std::endl;
+        
+        mi->comm->requestNewCalibration();
 
         repl::sleep(1);
 
-        if (!mi->comm->isCalibrationInProgress()) mi->comm->requestNewCalibration();
+        std::string err;
+        mi->comm->allowMotorsCalibrationToStart(1, err);
+        OUTPUT_WARNING("%s", err.c_str());
 
+        repl::sleep(1);
+        // if (!mi->comm->isCalibrationInProgress()) mi->comm->requestNewCalibration();
         while (mi->comm->isCalibrationInProgress()) repl::sleep(0.25);
 
         repl::sleep(1);
@@ -32,14 +33,18 @@ void controlLoop()
         repl::sleep(1);
 
         OUTPUT_INFO("Writing position");
-        mi->cmd[0] = 1; mi->cmd[1] = 1; mi->cmd[2] = 1; mi->cmd[3] = 1; mi->cmd[4] = 1; mi->cmd[5] = 1; mi->cmd[6] = 1;
-        for (int i = 0; i < 10000; ++i) {
-            // OUTPUT_INFO("Connection %d", mi->comm->isConnectionOk());
-            mi->read();
-            mi->write();
-            repl::sleep(0.01);
-        }
-        bool ok = true;
+        mi->cmd[0] = 1; mi->cmd[1] = 1; mi->cmd[2] = 1; mi->cmd[3] = 1; mi->cmd[4] = 1; mi->cmd[5] = 1; mi->cmd[6] = 1;        
+        mi->write();
+
+        repl::sleep(10);
+
+        mi->cmd[0] = 0; mi->cmd[1] = 0; mi->cmd[2] = 0; mi->cmd[3] = 0; mi->cmd[4] = 0; mi->cmd[5] = 0; mi->cmd[6] = 0;
+        mi->write();
+
+        repl::sleep(10);
+
+        mi->comm->activateLearningMode(true);
+
         repl::sleep(10);
     }
 
